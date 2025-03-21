b1406ed812ba163cd8b3e21dff0c7086ebd731d5
maxubrq <hungtp.play@gmail.com>
2025-03-21 22:29:19 +0700

    :tada: init

---
+++ misc/default-metadata.ts
@@ -0,0 +1,7 @@
+import { Metadata } from "next";
+
+export const DEFAULT_METADATA: Metadata = {
+    title: '100 Motion Challenges (Or more)',
+    description: 'A collection of 100 motion challenges to help you improve your animation skills.',
+    keywords: 'animation, challenges, motion, skills',
+}
